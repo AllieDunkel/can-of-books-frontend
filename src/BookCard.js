d7123e5377a3c12648c9ab22bd035a0b6e6a5647
jordanLindo <jordanlindo@live.com>
2022-08-10 17:14:35 -0500

    Working on update DO NOT MERGE

---
+++ src/BookCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, Button } from 'react-bootstrap';
+
+class BookCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            book: this.props.book
+        }
+    }
+
+
+
+
+    render() {
+        let book = this.state.book;
+        return (
+            <>
+                <Card className="row">
+                    <Card.Title className='col-sm'>{book.title}</Card.Title>
+                    <Card.Text className='col-md'>{book.description}</Card.Text>
+                    <Card.Text className='col-sm'>
+                        {book.available ? 'Available' : 'Unavailable'}
+                    </Card.Text>
+                    <Button className='col-sm' onClick={() => this.props.handleOnShowModal(this.state.book)}>Update</Button>
+                </Card>
+            </>
+        )
+    }
+}
+
+export default BookCard;
